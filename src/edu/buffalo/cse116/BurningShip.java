3b5b5914cc8bff8d37198e8f347408ed0443cca5
Brian Prais <brianandrew.prais@yahoo.com>
2017-02-23 00:16:03 -0500

    BurningShip Class

---
+++ src/edu/buffalo/cse116/BurningShip.java
@@ -0,0 +1,54 @@
+ackage edu.buffalo.cse116;
+
+import java.awt.Point;
+
+public class BurningShip {
+	
+	int[][] _grid = new int[512][512];
+	
+	public BurningShip(){
+
+		for (int row = 0; row <= 512; row++){
+			for (int column = 0; column <= 512; column++){
+				Coord c = new Coord((-1.8 + (row * .00431640625)), (-1.7 + (column * .005078125)));
+				// row side is domain/512 column side is range/512
+				_grid[row][column] = escapeTime(c);
+			}		
+			}
+	}
+		// Calculates escape time
+		public int escapeTime(Coord calc){
+			
+			double currentX = calc.x();
+			
+			double currentY = calc.y();
+			
+			double xCalc = currentX;
+			
+			double yCalc = currentY;
+			
+			double dist = Math.sqrt(Math.pow((xCalc - 0), 2) + Math.pow(yCalc - 0, 2));
+			
+			int passes = 0;
+			
+			while(dist <= 2 && passes < 255){
+				
+				xCalc = Math.pow(xCalc, 2) - Math.pow(yCalc, 2) + currentX;
+				
+				yCalc = Math.abs(2 * xCalc * yCalc) + currentY;
+				
+				passes = passes + 1;
+				
+				dist = Math.sqrt(Math.pow((xCalc - 0), 2) + Math.pow(yCalc - 0, 2));
+			}
+			
+			return passes;
+		}
+		
+		// returns escape time at a specific point in our grid
+		public int gridEscapeTime(Point p){
+		
+			return _grid[p.x][p.y];
+		}
+
+}
