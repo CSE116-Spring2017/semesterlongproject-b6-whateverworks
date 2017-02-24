3a6bbad3a1e2b82498da7fa1a1f9bfe49cf414d0
StevenLeFebvre <StevenLeFebvre@10.84.50.168>
2017-02-24 15:06:20 -0500

    almost there

--- src/edu/buffalo/cse116/Test5.java
+++ src/edu/buffalo/cse116/Test5.java
@@ -2,13 +2,49 @@ package edu.buffalo.cse116;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
+/** Delete this before submission
+* Im having trouble with the second assert statement for each test
+*/
 import org.junit.Test;
-
+// This class determines if the fractal's arrays are the proper size
 public class Test5 {
-
+// BurningShip
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void BurningShipSizetest() {
+		int arr[][] = BurningShip.calcFrac();
+		assertEquals(512, arr.length);
+		assertEquals(512, arr.get(0).length);
+		
+		
 	}
+  //Julia
+	@Test
+	public void JuliaSizetest() {
+		int arr[][] = Julia.calcFrac();
+		assertEquals(512, arr.length);
+		assertEquals(512, arr.get(0).length);
+		
+		
+	}
+  //Mandelbrot
+	@Test
+	public void MandelbrotSizetest() {
+		int arr[][] = Mandelbrot.calcFrac();
+		assertEquals(512, arr.length);
+		assertEquals(512, arr.get(0).length);
+		
+		
+	}
+  // Multibrot
+	@Test
+	public void MultibrotSizetest() {
+		int arr[][] = Multibrot.calcFrac();
+		assertEquals(512, arr.length);
+		assertEquals(512, arr.get(0).length);
+		
+		
+	}
+	
 
 }
